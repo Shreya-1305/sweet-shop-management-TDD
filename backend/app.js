13184a1a305f4fcda20a1cd7829daea128cf740d
Shreya-1305 <paintershreya@gmail.com>
2025-09-20 03:52:06 +0530

    feat: add catch-all 404 handler for undefined routes

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,12 @@ app.use("/api/auth", authRoutes);
 
 app.use("/api/sweets", sweetRoutes);
 
-// app.use(errorHandler);
+app.use("*", (req, res) => {
+  return sendError(res, 404, "Route not found");
+});
+
+app.use((err, req, res, next) => {
+  sendError(res, err.status || 500, err.message || "Server error");
+});
 
 module.exports = app;
