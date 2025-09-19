ea464f492f848acbd52923c487da7550172395a0
Shreya-1305 <paintershreya@gmail.com>
2025-09-20 05:01:46 +0530

    feat(frontend): Setup basic routing for Sweet Shop

---
+++ frontend/AdminProtectedRoute.jsx
@@ -0,0 +1,5 @@
+function AdminProtectedRoute() {
+  return <div></div>;
+}
+
+export default AdminProtectedRoute;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,52 @@
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Pages
+import LandingPage from "./pages/LandingPage";
+import PurchasePage from "./pages/PurchasePage";
+import AdminDashboard from "./pages/AdminDashboard";
+import LoginPage from "./pages/LoginPage";
+import ErrorPage from "./components/ErrorPage";
+
+// Protected Route wrappers
+import ProtectedRoute from "./ProtectedRoute";
+import AdminProtectedRoute from "./AdminProtectedRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />, // basic login page
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/purchase",
+    element: (
+      <ProtectedRoute>
+        <PurchasePage />
+      </ProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/admin",
+    element: (
+      <AdminProtectedRoute>
+        <AdminDashboard />
+      </AdminProtectedRoute>
+    ),
+    errorElement: <ErrorPage />,
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return (
+    <RouterProvider router={router} future={{ v7_startTransition: true }} />
+  );
 }
 
 export default App;
