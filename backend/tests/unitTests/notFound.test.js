4f64521dd72e496aeddaf31c1f730e73c08123bc
Shreya-1305 <paintershreya@gmail.com>
2025-09-20 03:49:25 +0530

    test: add failing unit tests for catch-all 404 route using sendError

---
+++ backend/tests/unitTests/notFound.test.js
@@ -0,0 +1,24 @@
+const request = require("supertest");
+const app = require("../../app");
+const sendError = require("../../utils/sendError");
+
+// Mock sendError so we can check it was called
+jest.mock("../../utils/sendError");
+
+describe("Catch-all 404 route", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return 404 for GET request on unknown route", async () => {
+    const res = await request(app).get("/non-existent-route");
+
+    expect(sendError).toHaveBeenCalledWith(res, 404, "Route not found");
+  });
+
+  it("should return 404 for POST request on unknown route", async () => {
+    const res = await request(app).post("/another-fake-route");
+
+    expect(sendError).toHaveBeenCalledWith(res, 404, "Route not found");
+  });
+});
